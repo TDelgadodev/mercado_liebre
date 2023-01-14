b98eb6af6807323d48968fd79527e12f1d265c8e
Thiago <thiagodelgado.dev@gmail.com>
2023-01-13 21:26:38 -0300

    box shadow, hovers and animations done!

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = 3030;
 
 app.use(express.static(path.resolve(__dirname,"public")))
 
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname,"views","home.html")));
-app.listen(3030,()=> "Servidor funcionando")
+app.get("/login", (req,res) => res.sendFile(path.resolve(__dirname,"views","login.html")));
+app.get("/register", (req,res) => res.sendFile(path.resolve(__dirname,"views","register.html")));
+
+app.listen(port, () => console.log(`Server Running in: http://localhost:${port}`));
